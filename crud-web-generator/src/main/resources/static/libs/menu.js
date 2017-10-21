d4d87ccda5320b7328e2b64b153f15706313e626
Yann Demel <ydemel@octo.com>
2017-10-21 16:20:10 +0200

    Side bar auto hide

--- crud-web-generator/src/main/resources/static/libs/menu.js
+++ crud-web-generator/src/main/resources/static/libs/menu.js
@@ -1,26 +1,50 @@
 $(document).ready(function () {
   var trigger = $('.hamburger'),
       overlay = $('.overlay'),
+	  wrapper = $('#wrapper'),
+	  sidebar = $('#sidebar-wrapper'),
      isClosed = false;
 
     trigger.click(function () {
       hamburger_cross();      
     });
+	
+	trigger.on({
+			mouseenter: function(){
+			hamburger_cross(); 			
+		}
+	});
 
     function hamburger_cross() {
 
       if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
+        closeMenu();
       } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
+		openMenu();        
+      }	  
   }
+  
+  function closeMenu() {
+	overlay.hide();
+	trigger.removeClass('is-open');
+	trigger.addClass('is-closed');	
+	wrapper.removeClass('toggled');
+	isClosed = false;
+  }
+  
+  function openMenu() {
+	overlay.show();
+	trigger.removeClass('is-closed');
+	trigger.addClass('is-open');	
+	wrapper.addClass('toggled');
+	isClosed = true;	
+  }
+  
+  sidebar.on({
+    mouseleave: function(){
+      closeMenu();
+    }    
+});
   
   $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
