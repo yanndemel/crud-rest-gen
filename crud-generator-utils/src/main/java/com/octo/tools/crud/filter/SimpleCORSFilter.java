230db730228ee92eff4b1afed0e22a65aee9751c
Yann_Demel <y.demel@cheuvreux.fr>
2019-04-12 16:04:45 +0200

    Add ETag and IF-Match in authorized requests/responses headers

--- crud-generator-utils/src/main/java/com/octo/tools/crud/filter/SimpleCORSFilter.java
+++ crud-generator-utils/src/main/java/com/octo/tools/crud/filter/SimpleCORSFilter.java
@@ -36,7 +36,8 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH, HEAD");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, If-Match, Authorization");
+        response.setHeader("Access-Control-Expose-Headers", "Cache-Control, Content-Language, Content-Type, Expires, ETag, Last-Modified, Pragma");
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
