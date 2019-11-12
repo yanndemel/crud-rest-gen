73d08e1253561665337d7e1fd63183aca803e02a
Yann_Demel <y.demel@cheuvreux.fr>
2019-11-12 17:52:25 +0100

    Revert "Set dirty context for tests"
    This reverts commit 69feeade0f19b50a33e851f4cb12df8838152157.

--- crud-generator-utils-tests/src/main/java/com/octo/tools/crud/doc/BaseApiDocumentation.java
+++ crud-generator-utils-tests/src/main/java/com/octo/tools/crud/doc/BaseApiDocumentation.java
@@ -75,10 +75,12 @@ public class BaseApiDocumentation extends AbstractCrudTest {
 
 	@Test
 	public void indexExample() throws Exception {
+		
 		getMockMvc("", HttpMethod.GET).perform(get(""))
 			.andExpect(status().isOk())
 			.andDo(print()).andDo(document("index-example",
 					links(linkResources())));
+		
 	}
 
 	private LinkDescriptor[] linkResources() throws ClassNotFoundException {
--- sample-app/petclinic-all/src/test/java/com/octo/tools/samples/petclinic/PetClinicEntitiesApiDocumentation.java
+++ sample-app/petclinic-all/src/test/java/com/octo/tools/samples/petclinic/PetClinicEntitiesApiDocumentation.java
@@ -1,13 +1,10 @@
 package com.octo.tools.samples.petclinic;
 
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 
 import com.octo.tools.crud.doc.EntitiesApiDocumentation;
 
 @ContextConfiguration(classes = Application.class)
-@DirtiesContext(classMode=ClassMode.AFTER_CLASS)
 public class PetClinicEntitiesApiDocumentation extends EntitiesApiDocumentation {
 
 
